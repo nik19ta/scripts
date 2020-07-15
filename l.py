3e1ed5e366ccd7f699602d998afe210d8cb55a89
Nikita Khvatov <nikhvatov19@gmail.com>
2020-07-15 10:25:51 +0300

    Иправил баги, и теперь показываются гит файлы

--- l.py
+++ l.py
@@ -6,19 +6,20 @@ try: command = argv[1].lower()
 except: command = False
 
 
-print(command)
 
 def out_w(text):
     print("\033[37m{}".format(text))
 
 try: git_data = git("diff", "--name-only")
-except: git_data = False
+except: git_data = ''
 try: git_untracked = str(git("status").split('(use "git add <file>..." to include in what will be committed)')[1].split('no changes added to commit (use "git add" and/or "git commit -a")')[0])
-except: git_untracked = False
+except: git_untracked = ''
 try: git_modified = str(git("status").split('(use "git reset HEAD <file>..." to unstage)')[1].split('Changes not staged for commit:')[0])
-except: git_modified = False
+except: git_modified = ''
+try: gitignore = str(git("status").split('(use "git add <file>..." to include in what will be committed)')[1].split('no changes added to commit (use "git add" and/or "git commit -a")')[0])
+except: gitignore = ''
 
-print(type(command))
+
 if command == "-a":
     files = ls("-la").splitlines()
 else:
@@ -29,32 +30,27 @@ if git_data:
     print(f'Путь: {pwd()}\033[32mОбноружен git ')
     print(' ')
 
-    files = ls("-l").splitlines()
-
     for line in files:
         mylist = [x for x in line.split(' ') if x]
 
         if len(mylist) > 2:
-            try: 
+            if mylist[8] == '.' or mylist[8] == '..':
+                pass
+            else:
                 if mylist[8] in git_data: 
                     out_w(f'\033[31m:modified  \033[37m{mylist[6].title()} {mylist[7]}, file:\033[31m{mylist[8]} ')
                     continue
-
-            except: pass
-            try: 
-                if mylist[8] in git_untracked: 
+                elif '.git' in mylist[8]: 
+                    out_w(f'\033[94m:git file \033[37m {mylist[6].title()} {mylist[7]}, file:\033[94m{mylist[8]}')
+                    continue
+                elif mylist[8] in git_untracked: 
                     out_w(f'\033[33m:untracked \033[37m{mylist[6].title()} {mylist[7]}, file:\033[33m{mylist[8]}')
                     continue
-            except: pass
-            try: 
-                if mylist[8] in git_modified: 
+                elif mylist[8] in git_modified: 
                     out_w(f'\033[32m:modified \033[37m {mylist[6].title()} {mylist[7]}, file:\033[32m{mylist[8]}')
                     continue
-            except: pass
-            try: 
-                out_w(f'\033[36m:commited \033[37m {mylist[6].title()} {mylist[7]}, file:\033[36m{mylist[8]}')
-                continue
-            except: pass
+                else:
+                    out_w(f'\033[36m:commited \033[37m {mylist[6].title()} {mylist[7]}, file:\033[36m{mylist[8]}')
 else:
 
     print(' ')
